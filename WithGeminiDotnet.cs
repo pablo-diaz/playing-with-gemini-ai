79fef938279cf5fd6799b6a6c7f228c9baf88885
Pablo Andrés Díaz <pabloandresd@gmail.com>
2025-08-04 15:55:46 -0500

    Adding another scenario to run Agent Workflows

--- WithGeminiDotnet.cs
+++ WithGeminiDotnet.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 
@@ -40,9 +40,11 @@ internal class PlayingWithGeminiAI
 
         Console.WriteLine("**********************************************************************");
         Console.WriteLine("Running scenarios with Gemini AI, using GeminiDotnet which implements Microsoft.Extensions.AI ...");
+        
         await RunBasicScenario(client);
         await RunScenarioWithTools(client);
         await RunScenarioAboutStructuredOutput(client);
+        await RunScenarioForWorkflow(client);
     }
 
     private static async Task RunBasicScenario(IChatClient client)
@@ -99,6 +101,66 @@ internal class PlayingWithGeminiAI
             ideasAboutArticle.Print();
     }
 
+    private static async Task RunScenarioForWorkflow(IChatClient client)
+    {
+        var options = new ChatOptions
+        {
+            Tools = [
+                AIFunctionFactory.Create(method: ToolsThatCanBeInvokedByLLMs.GetOrderInformation, name: nameof(ToolsThatCanBeInvokedByLLMs.GetOrderInformation)),
+                AIFunctionFactory.Create(method: ToolsThatCanBeInvokedByLLMs.SendOrderToBeReviewedByManager, name: nameof(ToolsThatCanBeInvokedByLLMs.SendOrderToBeReviewedByManager)),
+                AIFunctionFactory.Create(method: ToolsThatCanBeInvokedByLLMs.PrintOrder, name: nameof(ToolsThatCanBeInvokedByLLMs.PrintOrder)),
+                AIFunctionFactory.Create(method: ToolsThatCanBeInvokedByLLMs.LogReasonWhyStepCannotBePerformed, name: nameof(ToolsThatCanBeInvokedByLLMs.LogReasonWhyStepCannotBePerformed)),
+                AIFunctionFactory.Create(method: ToolsThatCanBeInvokedByLLMs.CancelOrder, name: nameof(ToolsThatCanBeInvokedByLLMs.CancelOrder))
+            ]
+        };
+
+        var agentInstructions = """
+                You are a helpful assistant that helps people to get their input, enhance its data with the tools (functions) you are provided with, and then return a structured output.
+                
+                These are the steps you **must** follow:
+                - First you take an input from the user, which is a request in Spanish.
+                - Then you use the tools (functions) provided to you, to get more context regarding what the user requested.
+                - You should also use the tools (functions) provided to you, to perform the actions the user requested.
+                - Try to avoid skipping any of the steps the user requested you to perform.
+                - Do not answer the user until you have all the information you need to provide a complete answer.
+                - If any of the steps you are asked to perform cannot be done, you must log the technical reason why it cannot be done, using the `LogReasonWhyStepCannotBePerformed` function. Please try to be as specific as possible with this reason.
+
+                Please respond with a very natural language answer in Spanish:
+                - Use very informal expressions please, that summarizes the steps you took to get the information you are providing to the user
+                - If possible, please use Colombian slangs, such as Listo, Pillar, Parce, Chévere, Bacano, etc.
+                - Also, if possible, please use emojis to make the answer more friendly and engaging
+            """;
+
+        string[] userMessagesToTriggerEachWorkflow = [
+            """
+                Hola, me ayudas porfa con lo siguiente:
+                1. Quisiera saber cómo va mi Orden XYZ123
+                2. Luego envíala a ser revisada por mi jefe
+                3. Y por último imprímela
+
+                Muchas gracias
+            """,
+
+            """
+                Por favor averigua el estado actual de mi orden ABC00987, luego porfa la Cancelas y por último, **solo luego que la canceles**, porfa imprímela
+            """
+        ];
+
+        var clientEnhancedForFunctionCalling = EnhanceClientToAllowFunctionCalling(fromClient: client);
+        foreach(var userMessage in userMessagesToTriggerEachWorkflow)
+        {
+            List<ChatMessage> messages = [
+                new(ChatRole.System, agentInstructions),
+                new(ChatRole.User, userMessage)
+            ];
+
+            var response = await clientEnhancedForFunctionCalling.GetResponseAsync(messages, options);
+
+            PrintMessageFromAiAssistant(message: response.Text);
+            Console.WriteLine("------------------------------------------------------");
+        }
+    }
+
     private static IChatClient GetClient(string withApiKey) =>
         new GeminiChatClient(options: new() {
             ApiKey = withApiKey,
@@ -106,7 +168,7 @@ internal class PlayingWithGeminiAI
             ModelId = "gemini-2.5-flash",
         });
 
-    private static IChatClient EnhanceClientToAllowFunctionCalling(IChatClient fromClient, ILoggerFactory withLoggerFactory) =>
+    private static IChatClient EnhanceClientToAllowFunctionCalling(IChatClient fromClient, ILoggerFactory withLoggerFactory = null) =>
         new ChatClientBuilder(fromClient)
             .UseFunctionInvocation(loggerFactory: withLoggerFactory)
             .Build();
